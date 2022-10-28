86d25d83897318be616392f1f81901f97304d14d
zzzdong <kuwater@163.com>
2022-10-28 17:42:27 +0800

    feat: improve client log

--- src/client.rs
+++ src/client.rs
@@ -7,8 +7,8 @@ use tokio::{
     io::AsyncWriteExt,
     net::{tcp::OwnedWriteHalf, TcpStream},
 };
-use tokio_util::codec::{BytesCodec, Framed, FramedParts, FramedRead};
-use tracing::{debug, error, info, trace, Instrument};
+use tokio_util::codec::{BytesCodec, Framed, FramedRead};
+use tracing::{debug, error, info, info_span, trace, Instrument};
 
 use crate::{
     codecs::socks5::{CmdCodec, HandshakeCodec},
@@ -46,32 +46,18 @@ pub async fn start_client(
         .unwrap_or_else(|e| panic!("can not bind {}, {:?}", addr, e));
 
     loop {
-        let (socket, remote_addr) = listener.accept().await.expect("accpet failed");
+        let (socket, income_addr) = listener.accept().await.expect("accpet failed");
 
         let mut conn = ProxyHandler::new(url.clone(), http_client.clone());
 
         tokio::spawn(
-            async move {
-                match conn.handshake(socket).await {
-                    Ok((body, write_half)) => {
-                        match ProxyHandler::streaming(body, write_half).await {
-                            Ok(_) => {
-                                info!("proxy streaming done")
-                            }
-                            Err(err) => {
-                                error!("proxy streaming error, {:?}", err);
-                            }
-                        }
-                    }
-                    Err(e) => error!("proxy handshake error, {:?}", e),
-                }
-            }
-            .instrument(tracing::info_span!("conn", %remote_addr)),
+            async move { conn.handle(socket).await.unwrap() }
+                .instrument(info_span!("conn", %income_addr)),
         );
     }
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 struct ProxyHandler {
     remote: String,
     http: hyper::Client<NoiseConnector, Body>,
@@ -85,134 +71,137 @@ impl ProxyHandler {
         }
     }
 
-    pub async fn handshake(
-        &mut self,
-        socket: TcpStream,
-    ) -> Result<(Option<Body>, OwnedWriteHalf), anyhow::Error> {
-        let cmd = self.accept_socks5(socket).await?;
+    pub async fn handle(&mut self, socket: TcpStream) -> Result<(), anyhow::Error> {
+        let (cmd, stream) = self.accept_socks5(socket).await?;
 
-        self.connect_dest(cmd).await.map_err(Into::into)
+        let dest = dest_addr(&cmd)?;
+
+        async move {
+            match self.forward(cmd, stream).await {
+                Ok(_) => {
+                    info!("handle socket finished");
+                }
+                Err(err) => {
+                    error!("handle socket error: {:?}", err);
+                }
+            };
+
+            Ok::<_, anyhow::Error>(())
+        }
+        .instrument(info_span!("dest", %dest))
+        .await
+        .unwrap();
+
+        Ok(())
     }
 
-    async fn accept_socks5(&self, socket: TcpStream) -> Result<CmdFramed, RsocksError> {
-        let (framed, mut stream) = Framed::new(socket, HandshakeCodec).into_future().await;
-        let req = match framed {
-            Some(req) => req,
-            None => return Err(socks_error("read request failed")),
-        }?;
+    async fn forward(&mut self, cmd: CmdRequest, stream: CmdFramed) -> Result<(), anyhow::Error> {
+        let (body, write_half) = self.connect_dest(cmd, stream).await?;
 
-        let resp = HandshakeResponse::new(SOCKS5_AUTH_METHOD_NONE);
-        stream.send(resp).await?;
+        ProxyHandler::streaming(body, write_half).await
+    }
+
+    async fn accept_socks5(
+        &self,
+        socket: TcpStream,
+    ) -> Result<(CmdRequest, CmdFramed), RsocksError> {
+        // handshake frame
+        let mut stream = Framed::new(socket, HandshakeCodec);
+
+        let req = stream
+            .next()
+            .await
+            .ok_or_else(|| socks_error("read socks5 request failed"))??;
 
         if !req.methods.contains(&SOCKS5_AUTH_METHOD_NONE) {
             return Err(socks_error("method not support"));
         }
 
-        let FramedParts {
-            io,
-            read_buf,
-            write_buf,
-            ..
-        } = stream.into_parts();
+        let resp = HandshakeResponse::new(SOCKS5_AUTH_METHOD_NONE);
+        stream.send(resp).await?;
 
-        let mut new_parts = FramedParts::new(io, CmdCodec);
+        // cmd frame
+        let mut stream = stream.map_codec(|_| CmdCodec);
 
-        new_parts.write_buf = write_buf;
-        new_parts.read_buf = read_buf;
+        let req = stream
+            .next()
+            .await
+            .ok_or_else(|| socks_error("read socks5 cmd failed"))??;
 
-        let cmd = Framed::from_parts(new_parts);
-
-        Ok(cmd)
+        Ok((req, stream))
     }
 
     async fn connect_dest(
         &mut self,
-        stream: CmdFramed,
-    ) -> Result<(Option<Body>, OwnedWriteHalf), RsocksError> {
-        let (framed, mut stream) = stream.into_future().await;
-
-        let req = match framed {
-            Some(req) => req,
-            None => return Err(socks_error("read request failed")),
-        }?;
-
-        debug!("cmd request: {:?} from {:?}", req, stream);
-        let CmdRequest { address, port, .. } = req.clone();
+        cmd: CmdRequest,
+        mut stream: CmdFramed,
+    ) -> Result<(Body, OwnedWriteHalf), RsocksError> {
+        debug!("cmd request: {:?}", cmd);
+        let CmdRequest { address, port, .. } = cmd.clone();
 
         // only support TCPConnect
-        if req.command != Command::TCPConnect {
+        if cmd.command != Command::TCPConnect {
             let resp = CmdResponse::new(Reply::CommandNotSupported, address, port);
             stream.send(resp).await?;
             return Err(socks_error("command not support"));
         }
 
-        let FramedParts { io, .. } = stream.into_parts();
-
-        let (read_half, mut write_half) = io.into_split();
+        let (read_half, mut write_half) = stream.into_inner().into_split();
 
         let stream = FramedRead::new(read_half, BytesCodec::new());
 
-        let addr = dest_addr(&req)?;
+        let dest = dest_addr(&cmd)?;
 
         // use http2
         let h2_req = hyper::Request::builder()
             .version(hyper::http::Version::HTTP_2)
             .uri(&self.remote)
-            .header(DEST_ADDR, addr)
+            .header(DEST_ADDR, &dest)
             .body(Body::wrap_stream(stream))
             .unwrap();
 
-        let CmdRequest { address, port, .. } = req.clone();
+        let CmdRequest { address, port, .. } = cmd.clone();
 
         let mut socks_resp = CmdResponse::new(Reply::GeneralFailure, address, port);
 
-        let (cmd_resp, body) = match self.http.request(h2_req).await {
-            Ok(resp) => {
-                trace!("connected {:?}", req.address);
+        let ret = self.http.request(h2_req).await.map(|resp| {
+            trace!("connected {:?}", cmd.address);
 
-                let (parts, body) = resp.into_parts();
+            let (parts, body) = resp.into_parts();
 
-                if !parts.status.is_success() {
-                    (socks_resp, None)
-                } else {
-                    socks_resp.reply = Reply::Succeeded;
-
-                    (socks_resp, Some(body))
-                }
+            if parts.status.is_success() {
+                socks_resp.reply = Reply::Succeeded;
             }
-            Err(err) => {
-                error!(%err, "connect remote failed");
 
-                socks_resp.reply = Reply::HostUnreachable;
-
-                (socks_resp, None)
-            }
-        };
+            body
+        });
+        if let Err(ref _err) = ret {
+            socks_resp.reply = Reply::HostUnreachable;
+        }
 
         let mut buf = BytesMut::new();
-        cmd_resp.write_to(&mut buf);
+        socks_resp.write_to(&mut buf);
         write_half.write_buf(&mut buf).await?;
 
-        Ok((body, write_half))
+        ret.map_err(|err| socks_error(format!("connect {dest} failed, {err}")))
+            .map(|body| (body, write_half))
     }
 
     async fn streaming(
-        stream_body: Option<Body>,
+        mut stream_body: Body,
         mut write_half: OwnedWriteHalf,
     ) -> anyhow::Result<()> {
-        if let Some(mut body) = stream_body {
-            while let Some(data) = body.next().await {
-                match data {
-                    Ok(mut bs) => {
-                        if let Err(err) = write_half.write_buf(&mut bs).await {
-                            error!(%err, "send body buf failed");
-                            return Err(err.into());
-                        }
-                    }
-                    Err(err) => {
-                        error!(%err, "recv body data failed");
+        while let Some(data) = stream_body.next().await {
+            match data {
+                Ok(mut bs) => {
+                    if let Err(err) = write_half.write_buf(&mut bs).await {
+                        error!(%err, "send body buf failed");
                         return Err(err.into());
                     }
+                }
+                Err(err) => {
+                    error!(%err, "recv body data failed");
+                    return Err(err.into());
                 }
             }
         }
